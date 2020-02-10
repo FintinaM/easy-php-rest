f9ce3b89f8497101fcaf5638e980f0863d39bef2
Fintina Madalin <fintina.bogdan@gmail.com>
2020-02-10 23:04:27 +0200

    More post install tests

--- src/Composer/PostInstall.php
+++ src/Composer/PostInstall.php
@@ -1,10 +1,14 @@
 <?php
 namespace EasyPhpRest\Composer;
 
+
 class PostInstall
 {
  public static function createDir()
  {
      mkdir(__DIR__.'/test', 0755);
+     $TX = fopen('text.txt', 'r+');
+     fwrite($TX, '###############################');
+     fclose($TX);
  }
 }
